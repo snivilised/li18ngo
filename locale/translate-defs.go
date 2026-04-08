d746b6007bed3fff2b5c71285e91e26787ad2f52
plastikfan <plastikfan@outlook.com>
2026-04-08 20:58:42 +0100

    feat(locale): generate i18n code with lingo (#127)

--- locale/translate-defs.go
+++ locale/translate-defs.go
@@ -1,11 +1,18 @@
 package locale
 
 const (
+	// Li18ngoSourceID defines the ID (by convention the repo URL) required
+	// for i18n translation purposes.
 	Li18ngoSourceID = "github.com/snivilised/li18ngo"
+
+	// TestGrafficoSourceID test source ID
+	TestGrafficoSourceID = "github.com/snivilised/graffico"
 )
 
+// Li18ngoTemplData is the base template data for all messages in this repo.
 type Li18ngoTemplData struct{}
 
+// SourceID returns the source ID for this message.
 func (td Li18ngoTemplData) SourceID() string {
 	return Li18ngoSourceID
 }
--- locale/underlying.go
+++ locale/underlying.go
@@ -59,7 +59,125 @@ type Underliers map[string]UnderlyingTemplData
 // underliers is the single source of truth for all i18n messages in this
 // package. Edit this map and run go generate to regenerate the auto files.
 var underliers = Underliers{
-	//
+	// -------------------------------------------------------------------------
+	// General messages
+	// -------------------------------------------------------------------------
+	"using-config-file": {
+		MessageID:   "using-config-file",
+		Seed:        "UsingConfigFile",
+		TypeName:    enums.UnderlyingTypeDynamicGeneral,
+		Description: "Message to indicate which config is being used",
+		Story: "UsingConfigFile is printed on startup to indicate" +
+			" which configuration file has been loaded.",
+		Other: "Using config file: '{{.ConfigFileName}}'",
+		Fields: []UnderlyingField{
+			{
+				Note:   "ConfigFileName",
+				GoType: "string",
+				Tale:   "is the name of the config file being used",
+			},
+		},
+	},
+
+	"localisation.test": {
+		MessageID:   "localisation.test",
+		Seed:        "Localisation",
+		TypeName:    enums.UnderlyingTypeStaticGeneral,
+		Description: "Localisation",
+		Story:       "A test message for localisation.",
+		Other:       "localisation",
+	},
+
+	"internationalisation.test": {
+		MessageID:   "internationalisation.test",
+		Seed:        "Internationalisation",
+		TypeName:    enums.UnderlyingTypeStaticGeneral,
+		Description: "Internationalisation",
+		Story:       "A test message for internationalisation.",
+		Other:       "internationalisation",
+	},
+
+	"pavement-graffiti-report.graffico.test": {
+		MessageID:   "pavement-graffiti-report.graffico.test",
+		Seed:        "PavementGraffitiReportGrafficoTest",
+		TypeName:    enums.UnderlyingTypeDynamicGeneral,
+		Description: "Report of graffiti found on a pavement",
+		Story:       "A test message for pavement graffiti reporting.",
+		Other:       "Found graffiti on pavement; primary colour: '{{.Primary}}'",
+		Fields: []UnderlyingField{
+			{
+				Note:   "Primary",
+				GoType: "string",
+				Tale:   "is the primary colour of the graffiti",
+			},
+		},
+	},
+
+	"wrong-source-id.graffico.test": {
+		MessageID:   "wrong-source-id.graffico.test",
+		Seed:        "WrongSourceIDGrafficoTest",
+		TypeName:    enums.UnderlyingTypeStaticGeneral,
+		Description: "Message with wrong source ID for testing purposes",
+		Story:       "This message has the wrong source ID and should be ignored by i18n-gen.",
+		Other:       "This message should be ignored by i18n-gen.",
+	},
+
+	// -------------------------------------------------------------------------
+	// Error messages
+	// -------------------------------------------------------------------------
+
+	"path-not-found.dynamic-error": {
+		MessageID:   "path-not-found.dynamic-error",
+		Seed:        "PathNotFound",
+		TypeName:    enums.UnderlyingTypeDynamicError,
+		Description: "Directory or file path does not exist",
+		Story:       "PathNotFoundError is used when a directory or file path does not exist.",
+		Other:       "{{.Name}} path not found ({{.Path}})",
+		Fields: []UnderlyingField{
+			{
+				Note:   "Name",
+				GoType: "string",
+				Tale:   "is the name of the path that was not found (e.g. 'Config')",
+			},
+			{
+				Note:   "Path",
+				GoType: "string",
+				Tale:   "is the actual path that was not found (e.g. '/etc/config.yaml')",
+			},
+		},
+	},
+
+	"not-a-directory.dynamic-error": {
+		MessageID:   "not-a-directory.dynamic-error",
+		Seed:        "NotADirectory",
+		TypeName:    enums.UnderlyingTypeDynamicError,
+		Description: "File system path is not a directory",
+		Story:       "NotADirectoryError is used when a file system path is expected to be a directory but is not.",
+		Other:       "file system path '{{.Path}}', is not a directory",
+		Fields: []UnderlyingField{
+			{
+				Note:   "Path",
+				GoType: "string",
+				Tale:   "is the file system path that was expected to be a directory but is not (e.g. '/etc/config.yaml')",
+			},
+		},
+	},
+
+	"third-party.error-wrapper": {
+		MessageID:   "third-party.error-wrapper",
+		Seed:        "ThirdPartyErrorWrapper",
+		TypeName:    enums.UnderlyingTypeStaticErrorWrapper,
+		Description: "Wrapper for third-party errors",
+		Story:       "ThirdPartyErrorWrapper is used to wrap errors from third-party libraries.",
+		Other:       "An error occurred in a third-party library: {{.Wrapped}}",
+		Fields: []UnderlyingField{
+			{
+				Note:   "Wrapped",
+				GoType: "error",
+				Tale:   "is the original error from the third-party library that is being wrapped",
+			},
+		},
+	},
 }
 
 func init() {
